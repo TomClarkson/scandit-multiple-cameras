2ae2515c9258adcf2dc47d24e3d36df34e4de06d
Thomas Clarkson <tom@designsight.co.nz>
2024-06-25 13:34:34 +1200

    add single and multi-scan

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,10 @@
-import { Button, SafeAreaView, Text, View } from "react-native";
+import {
+  Button,
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 import { useCallback, useState } from "react";
 import { CameraScannerCapture } from "@/scandit/CameraScannerCapture";
@@ -9,7 +15,8 @@ type ReadBarcode = {
 };
 
 export default function HomeScreen() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isSingleScanOpen, setIsSingleScanOpen] = useState(false);
+  const [isMultiScanOpen, setIsMultiScanOpen] = useState(false);
 
   const [readBarcodes, setReadBarcodes] = useState<ReadBarcode[]>([]);
 
@@ -19,26 +26,63 @@ export default function HomeScreen() {
   }, []);
 
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View style={{ flex: 1, backgroundColor: "pink" }}>
-        <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-          <Text style={{ fontSize: 20 }}>Barcodes</Text>
-          <Button onPress={() => setReadBarcodes([])} title="Clear" />
+    <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
+      <View style={{ flex: 1 }}>
+        <View style={{ padding: 8 }}>
+          <View
+            style={{ flexDirection: "row", justifyContent: "space-between" }}
+          >
+            <Text style={{ fontSize: 20 }}>Barcodes</Text>
+            <Button onPress={() => setReadBarcodes([])} title="Clear" />
+          </View>
+          <View>
+            {readBarcodes.map((barcode) => (
+              <Text key={barcode.data}>
+                {barcode.symbology} - {barcode.data}
+              </Text>
+            ))}
+          </View>
         </View>
-        <View>
-          {readBarcodes.map((barcode) => (
-            <Text key={barcode.data}>
-              {barcode.symbology} - {barcode.data}
+        <View style={{ flexDirection: "row", justifyContent: "space-around" }}>
+          <TouchableOpacity
+            disabled={isMultiScanOpen}
+            onPress={() => setIsSingleScanOpen((o) => !o)}
+            style={{
+              backgroundColor: "blue",
+              padding: 10,
+              borderRadius: 5,
+            }}
+          >
+            <Text style={{ color: "white" }}>
+              {isSingleScanOpen ? "Close Single Scan" : "Open Single Scan"}
             </Text>
-          ))}
+          </TouchableOpacity>
+          <TouchableOpacity
+            disabled={isSingleScanOpen}
+            onPress={() => setIsMultiScanOpen((o) => !o)}
+            style={{
+              backgroundColor: "yellow",
+              padding: 10,
+              borderRadius: 5,
+            }}
+          >
+            <Text>
+              {isMultiScanOpen ? "Close Multi Scan" : "Open Multi Scan"}
+            </Text>
+          </TouchableOpacity>
         </View>
 
-        <Button
-          onPress={() => setIsOpen((o) => !o)}
-          title={isOpen ? "Close Camera" : "Open Camera"}
-        />
-        {isOpen && (
-          <CameraScannerCapture onBarcodeScanned={handleBarcodeScanned} />
+        {isSingleScanOpen && (
+          <CameraScannerCapture
+            onBarcodeScanned={handleBarcodeScanned}
+            mode="singleScan"
+          />
+        )}
+        {isMultiScanOpen && (
+          <CameraScannerCapture
+            onBarcodeScanned={handleBarcodeScanned}
+            mode="multiScan"
+          />
         )}
       </View>
     </SafeAreaView>
